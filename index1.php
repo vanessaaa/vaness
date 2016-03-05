04d90147be20336c4871239fbc59ed0e73a45d4e
vanessa choqque <vaneytha@gmail.com>
2016-03-05 12:59:11 -0500

    primer trabajo

---
+++ index1.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet"href="estilos1.css">
+	<body>
+		    <div class= "contenedor">
+		        <nav>
+		        	<ul class="menu-lista">
+		        		<li class="menu-item"><a href="" class="menu-link">AMOR</a></li>
+		        		<li class="menu-item"><a href="" class="menu-link">ESTO</a></li>
+		        		<li class="menu-item"><a href="" class="menu-link">LO</a></li>
+		        		<li class="menu-item"><a href="" class="menu-link">HICE</a></li>
+		        		<li class="menu-item"><a href="" class="menu-link">PARA</a></li>
+                        <li class="menu-item"><a href="" class="menu-link">TI</a></li>
+                        <li class="menu-item"><a href="" class="menu-link">ESPERO</a></li>
+                        <li class="menu-item"><a href="" class="menu-link">QUE</a></li>
+                        <li class="menu-item"><a href="" class="menu-link">TE</a></li>
+                        <li class="menu-item"><a href="" class="menu-link">GUSTE</a></li>
+
+		        	</ul>
+		        </nav>
+		    </div class= "slider">
+		    <img src="9.jpg" heigth="8" width="1335">
+		    <img src="1.jpg.lnk" heigth="170" width="260">
+		    <img src="2.jpg" heigth="170" width="265">
+		    <img src="3.JPG.lnk" heigth="170" width="270">
+		    <img src="4.jpg" heigth="170" width="260">
+            <img src="5.JPG.lnk" heigth="170" width="260">
+		    <h1>Amor te extraño un montón sabes que te amo muchito  en estos días que nonos hemos comunicado ni por cel, ni por facebook  es un infierno al  no verte en línea me da ganas de llorar al no saber nada de ti, a veces cuando te llamo no me contestas y todo eso no sé hasta cuándo vamos a seguir así sin comunicarnos te juro que te extraño.
+
+Yo quiero que este amor que nos tenemos, que hay entre los dos  no quiero que termine nunca sip y a través de la distancia mis recuerdos viven en ti, y sólo te pido amor mio que no te olvides de mí.
+
+Una de las cosas más difíciles de este mundo es querer a alguien y no tenerlo a tu lado. Es en esos momentos donde no sabes que hacer porque todo lo que puedes hacer es simplemente esperar.
+
+Te esperare amor y espero que tú también lo agás y pronto estaré por allá amor ya te diré cuando sip amor te amooooo <3 <3   .
+</h1>
+		</body>
+		</html
